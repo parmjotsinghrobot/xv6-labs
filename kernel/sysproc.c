bb06f7d2b867428bb6c36960ca66a9ce2bc2409a
Parmjot Singh <parmjotsinghrobot@gmail.com>
2026-03-25 19:10:49 +1300

    feat(setpri): take process lock while writing priority

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -102,14 +102,19 @@ sys_uptime(void)
 uint64
 sys_setpri(void)
 {
+  acquire(&tickslock);
   int num;
   argint(0, &num);
 
-  struct proc *p = myproc();
   // return on invalid priorities
   if (!((num == 1) || (num == 2))) {
+    release(&tickslock);
     return -1;
   }
+
+  struct proc *p = myproc();
   p->priority = num;
+
+  release(&tickslock);
   return 0;
 }
